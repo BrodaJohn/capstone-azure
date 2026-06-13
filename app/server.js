6a1189e12dda84d29bd4f93c07d20d09b39e6657
BrodaJohn <topealade@gmail.com>
2026-06-13 19:03:01 +0100

    Add timestamp run app

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.send('<h1>This is group4 capstone project</h1><p> Capstone project 2026 group4 Deployed successfully via CI/CD + Docker + terraform</p>');
+  res.send('<h1>This is group4 capstone project</h1><p>Time stamp presentation7:02pm Naija time successfully via CI/CD + Docker + terraform</p>');
 });
 app.get('/health', (req, res) => res.json({ status: 'OK', platform: 'Azure' }));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

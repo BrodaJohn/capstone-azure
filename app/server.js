d0da034db817f6eceb26495594f1f93c95d79aa6
BrodaJohn <topealade@gmail.com>
2026-06-13 16:59:22 +0100

    Add GitHub Actions CI/CD workflow

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.send('<h1>This is group4 capstone project</h1><p> I started 5th June around 10pm, finished 06 June 0630am Deployed successfully via CI/CD + Docker + terraform</p>');
+  res.send('<h1>This is group4 capstone project</h1><p> Capstone project 2026 group4 Deployed successfully via CI/CD + Docker + terraform</p>');
 });
 app.get('/health', (req, res) => res.json({ status: 'OK', platform: 'Azure' }));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
